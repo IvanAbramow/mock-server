d1115639bd67f473ed7c2f9263f4358dab3d6a5e
Ivan Abramov <ivan.abramov@onetwotrip.com>
2022-08-05 14:23:07 +0300

    fix

--- client.js
+++ client.js
@@ -20,25 +20,3 @@ async function main() {
 }
 
 main();
-
-
-const { mockServerClient } = require('mockserver-client');
-
-const MOCKSERVER_HOST = 'localhost';
-const MOCKSERVER_PORT = 17189;
-
-const { mockAnyResponse, clear } = mockServerClient(MOCKSERVER_HOST, MOCKSERVER_PORT);
-
-async function main() {
-    await clear()
-
-    await mockAnyResponse({
-        httpForward: {
-            scheme: 'HTTP',
-            host: 'localhost',
-            port: 17188
-        }
-    });
-}
-
-main();
